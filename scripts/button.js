c4f20cdda9679c1103f2b170dcf464f292c4ef95
niklaus699 <nicholasbasseyy@gmail.com>
2025-01-10 22:18:08 +0100

    Initial commit for my website project

---
+++ scripts/button.js
@@ -0,0 +1,8 @@
+// Select the button element
+const button = document.getElementById('student-btn');
+
+// Add a click event listener to the button
+button.addEventListener('click', () => {
+    // Redirect to the target HTML file
+    window.location.href = 'performance/index.html'; // Replace with your target file's name
+});
